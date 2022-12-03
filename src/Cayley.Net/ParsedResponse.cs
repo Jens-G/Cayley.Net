65c54b897c2347f8b04182c26875a225ecc15dcf
Jens Geyer <jensg@apache.org>
2022-12-03 14:08:17 +0100

    added generic response object

---
+++ src/Cayley.Net/ParsedResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable IDE1006
+
+namespace Cayley.Net
+{
+    public class ParsedResponse
+    {
+        public List<Quad> result { get; set; }
+    }
+
+    public class Quad
+    {
+        public string id { get; set; }
+    }
+}
